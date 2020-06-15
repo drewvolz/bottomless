99107c31050a2a4f974d8075cea30cd86d480723
Drew Volz <volz.drew@gmail.com>
2020-06-15 15:15:40 -0700

    Add the common colors

---
+++ Bottomless/Constants/Colors.swift
@@ -0,0 +1,17 @@
+
+import SwiftUI
+import UIKit
+
+extension Color {
+    static let body = Color(.body)
+    static let pastelBlue = Color(.pastelBlue)
+    static let pastelYellow = Color(.pastelYellow)
+    static let pastelOrange = Color(.pastelOrange)
+}
+
+extension UIColor {
+    static let body = UIColor(red: 45 / 255, green: 49 / 255, blue: 63 / 255, alpha: 1)
+    static let pastelBlue = UIColor(red: 0.518, green: 0.639, blue: 0.925, alpha: 1)
+    static let pastelYellow = UIColor(red: 0.976, green: 0.933, blue: 0.000, alpha: 1)
+    static let pastelOrange = UIColor(red: 0.973, green: 0.345, blue: 0.243, alpha: 1)
+}
