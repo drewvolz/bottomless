d5ecade2be918bc3e03a805d1f7f195ff5ca5caa
Drew Volz <volz.drew@gmail.com>
2020-06-15 15:14:13 -0700

    Add the image extension

---
+++ Bottomless/Extensions/UIImage.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
