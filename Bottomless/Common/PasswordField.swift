993f3e23ca56dcb0e96a420cfc790b27ee5c0321
Drew Volz <volz.drew@gmail.com>
2020-06-15 15:17:45 -0700

    Add the password field UI

---
+++ Bottomless/Common/PasswordField.swift
@@ -0,0 +1,82 @@
+import SwiftUI
+
+struct PasswordField: View {
+    @Binding var value: String
+
+    var header = "Password"
+    var placeholder = "Your password"
+    var errorMessage = ""
+    var showUnderline = true
+    var onEditingChanged: ((Bool) -> Void) = { _ in }
+    var onCommit: (() -> Void) = {}
+
+    @State var isSecure: Bool = true
+
+    let pasteboard = UIPasteboard.general
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(header.uppercased())
+                .font(.footnote)
+                .foregroundColor(Color.black)
+
+            HStack {
+                ZStack {
+                    SecureField(placeholder, text: self.$value, onCommit: {
+                        self.onEditingChanged(false)
+                    })
+                        .padding(.vertical, 15)
+                        .opacity(isSecure ? 1 : 0)
+
+                    TextField(placeholder, text: self.$value, onEditingChanged: { flag in
+                        self.onEditingChanged(flag)
+                    })
+                        .padding(.vertical, 15)
+                        .opacity(isSecure ? 0 : 1)
+                }
+
+                HStack {
+                    if isSecure {
+                        Image(systemName: "eye.slash").foregroundColor(Color.gray).onTapGesture {
+                            withAnimation {
+                                self.isSecure.toggle()
+                            }
+                        }
+                    } else {
+                        Image(systemName: "doc.on.doc")
+                            .foregroundColor(Color.gray)
+                            .transition(.opacity)
+                            .onTapGesture {
+                                self.pasteboard.string = self.value
+                            }
+
+                        Image(systemName: "eye").foregroundColor(Color.gray).onTapGesture {
+                            withAnimation {
+                                self.isSecure.toggle()
+                            }
+                        }
+                    }
+                }
+            }.frame(height: 45)
+
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(Color.gray)
+
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .lineLimit(nil)
+                    .font(.footnote)
+                    .foregroundColor(Color.red)
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            }
+        }
+        .background(Color.white)
+    }
+}
+
+struct PasswordField_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordField(value: .constant(""))
+    }
+}
