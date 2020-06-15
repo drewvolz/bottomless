4d09c42f8b133cfc0aa006d7574ecfd5e9bc5098
Drew Volz <volz.drew@gmail.com>
2020-06-15 15:20:30 -0700

    Add the orders viewmodel

---
+++ Bottomless/View Models/OrdersViewModel.swift
@@ -0,0 +1,32 @@
+import Combine
+import SwiftUI
+
+final class OrdersViewModel: ObservableObject {
+    @Published private(set) var ordersResponse: [OrdersResponse]? = nil
+
+    private var ordersCancellable: Cancellable? {
+        didSet { oldValue?.cancel() }
+    }
+
+    deinit {
+        ordersCancellable?.cancel()
+    }
+
+    func fetch() {
+        let url = URL(string: App.api.orders)!
+
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: request)
+
+        ordersCancellable = publisher
+            .map { $0.data }
+            .decode(type: OrdersResultResponse.self, decoder: JSONDecoder())
+            .map { $0.data }
+            .replaceError(with: nil)
+            .receive(on: RunLoop.main)
+            .assign(to: \.ordersResponse, on: self)
+    }
+}
