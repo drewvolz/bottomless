3185d40816d16ad76965e0def8e6257e8870b20e
Drew Volz <volz.drew@gmail.com>
2020-06-16 15:23:42 -0700

    Add the credits viewmodel

---
+++ Bottomless/View Models/CreditsViewModel.swift
@@ -0,0 +1,32 @@
+import Combine
+import SwiftUI
+
+final class CreditsViewModel: ObservableObject {
+    @Published private(set) var creditsResponse: CreditsResponse?
+
+    private var creditsCancellable: Cancellable? {
+        didSet { oldValue?.cancel() }
+    }
+
+    deinit {
+        creditsCancellable?.cancel()
+    }
+
+    func fetch() {
+        let url = URL(string: App.api.credits)!
+
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: request)
+
+        creditsCancellable = publisher
+            .map { $0.data }
+            .decode(type: CreditsResponse.self, decoder: JSONDecoder())
+            .map { $0 }
+            .replaceError(with: nil)
+            .receive(on: RunLoop.main)
+            .assign(to: \.creditsResponse, on: self)
+    }
+}
