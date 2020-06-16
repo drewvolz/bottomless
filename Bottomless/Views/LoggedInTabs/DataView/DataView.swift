d526c99eea42269be78fffe5d03bcc5da7fd50b9
Drew Volz <volz.drew@gmail.com>
2020-06-16 05:07:17 -0700

    Add view to show chart and scale status information

--- Bottomless/Views/LoggedInTabs/DataView/DataView.swift
+++ Bottomless/Views/LoggedInTabs/DataView/DataView.swift
@@ -1,9 +1,76 @@
 import SwiftUI
+import SwiftUICharts
 
 struct DataView: View {
+    @ObservedObject var recordsViewModel = RecordsViewModel()
+    @ObservedObject var scaleViewModel = ScaleViewModel()
+
+    var weights: [Double]? {
+        recordsViewModel.recordsResponse.flatMap { record in
+            record.compactMap { $0.adjusted_weight }
+        }
+    }
+
     var body: some View {
-        Text("Data")
-            .font(.title)
+        Group {
+            List {
+                Group {
+                    Section {
+                        LineView(data: weights?.reversed() ?? [], title: "Scale readings")
+                    }
+                    // TODO: play with these paddings and make sure they work for all ranges
+                    .padding(.top, 140)
+                    .padding(.bottom, 180)
+
+                    Section(header: Text("Scale")) {
+                        HStack {
+                            Text("Last weight")
+                                .bold()
+                            Spacer()
+                            Text("\(String(format: "%.2f", scaleViewModel.scaleResponse?.scale_last_weight ?? 0))oz")
+                        }
+
+                        HStack {
+                            Text("Status")
+                                .bold()
+                            Spacer()
+                            Text("\(scaleViewModel.scaleResponse?.id?.uppercaseFirst() ?? "Unknown")")
+                        }
+
+                        HStack {
+                            Text("Last connected")
+                                .bold()
+                            Spacer()
+                            Text("\(dateFormatted())")
+                        }
+                    }
+                    .font(.body)
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .environment(\.horizontalSizeClass, .regular)
+        }
+        .onAppear(perform: fetch)
+    }
+
+    private func dateFormatted() -> String {
+        let relativeFormatter = RelativeDateTimeFormatter()
+        relativeFormatter.unitsStyle = .full
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+
+        let someDateTime = formatter.date(from: scaleViewModel.scaleResponse?.scale_last_connected ?? "")
+        let relativeDate = relativeFormatter.localizedString(for: someDateTime ?? Date(), relativeTo: Date())
+
+        if relativeDate == "in 0 seconds" { return "…" }
+
+        return relativeDate
+    }
+
+    private func fetch() {
+        recordsViewModel.fetch()
+        scaleViewModel.fetch()
     }
 }
 
