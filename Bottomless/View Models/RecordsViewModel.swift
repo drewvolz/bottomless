57c2fbeb4cc690af974e167a1166bff80fb70f2f
Drew Volz <volz.drew@gmail.com>
2020-06-16 05:06:22 -0700

    Add records viewmodel

---
+++ Bottomless/View Models/RecordsViewModel.swift
@@ -0,0 +1,32 @@
+import Combine
+import SwiftUI
+
+final class RecordsViewModel: ObservableObject {
+    @Published private(set) var recordsResponse: [RecordsResponse]?
+
+    private var recordsCancellable: Cancellable? {
+        didSet { oldValue?.cancel() }
+    }
+
+    deinit {
+        recordsCancellable?.cancel()
+    }
+
+    func fetch() {
+        let url = URL(string: App.api.records)!
+
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: request)
+
+        recordsCancellable = publisher
+            .map { $0.data }
+            .decode(type: RecordsResultResponse.self, decoder: JSONDecoder())
+            .map { $0.data }
+            .replaceError(with: nil)
+            .receive(on: RunLoop.main)
+            .assign(to: \.recordsResponse, on: self)
+    }
+}
