bd09b75ca0a3e7a849cf151a27091ddfbf411800
Drew Volz <volz.drew@gmail.com>
2020-06-18 23:39:34 -0700

    Remove unused model properties from the in-transition response

--- Bottomless/Models/Orders/InTransitionResponse.swift
+++ Bottomless/Models/Orders/InTransitionResponse.swift
@@ -8,48 +8,13 @@ struct InTransitionResultResponse: Decodable {
 struct InTransitionResponse: Hashable, Identifiable, Decodable {
     enum CodingKeys: String, CodingKey {
         case id = "_id"
-        case cost
-        case roastOptions = "roast_options"
-        case notesDeprecated = "notes_deprecated"
-        case trackingUpdates = "tracking_updates"
-        case predictions
-        case sentWithScale
-        case userID = "user_id"
-        case productID = "product_id"
         case originalProductID = "original_product_id"
-        case vendorID = "vendor_id"
-        case quantity
-        case dateInitiated = "date_initiated"
         case status
-        case source
-        case userGenerated = "user_generated"
         case grind
-        case createdAt = "created_at"
-        case updatedAt = "updated_at"
-        case __v
-        case fulfillmentErrors
-        case excludeFulfilmentDates = "exclude_fulfilment_dates"
-        case fulfillmentTimelines = "fulfillment_timelines"
-    }
-
-    struct FulfillmentErrors: Decodable, Hashable {
-        struct Smoke: Decodable, Hashable {
-            var date: String
-            var error: String? // NSNull -- TODO: fixme
-        }
-
-        var smoke: Smoke
-    }
-
-    struct FulfillmentTimelines: Decodable, Hashable {
-        var sunday, saturday, friday, thursday: Bool
-        var wednesday, tuesday, monday: Bool
     }
 
     struct Grind: Decodable, Hashable {
         var _id, name: String
-//        var default: String -- TODO: fixme
-        var __v: Int
     }
 
     struct OriginalProductID: Decodable, Hashable {
@@ -71,30 +36,10 @@ struct InTransitionResponse: Hashable, Identifiable, Decodable {
     struct VendorID: Decodable, Hashable {
         var _id: String
         var name: String
-        var fulfillment_timelines: FulfillmentTimelines
     }
 
     var id: String
-    var cost: Double
-    var roastOptions: [String]? // TODO: fixme
-    var notesDeprecated: [String]? // TODO: fixme
-    var trackingUpdates: [String]? // TODO: fixme
-    var predictions: [String]? // TODO: fixme
-    var sentWithScale: Bool
-    var userID: String
-    var productID: ProductID
     var originalProductID: OriginalProductID
-    var vendorID: VendorID
-    var quantity: String
-    var dateInitiated: String
     var status: String
-    var source: String
-    var userGenerated: Bool
     var grind: Grind
-    var createdAt: String
-    var updatedAt: String
-    var __v: Int
-    var fulfillmentErrors: FulfillmentErrors
-    var excludeFulfilmentDates: [String]
-    var fulfillmentTimelines: FulfillmentTimelines
 }
