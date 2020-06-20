0e3f06f08613f2d14f8f4dd49326cfdbf6ccfe35
Drew Volz <volz.drew@gmail.com>
2020-06-20 04:31:47 -0700

    Rename ordering segment to ordering strategy segment

---
+++ Bottomless/Views/LoggedInTabs/Account/OrderingStrategyView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct OrderingStrategyView: View {
+    @ObservedObject var accountViewModel: AccountViewModel
+    @ObservedObject var orderingStrategyViewModel = OrderingStrategy()
+
+    var optionTitles = ["Never run out", "Just right", "As fresh as possible"]
+    var orderingAggression: Int { accountViewModel.accountResponse?.orderingAggression ?? 2 }
+
+    init(accountViewModel: AccountViewModel) {
+        self.accountViewModel = accountViewModel
+    }
+
+    var body: some View {
+        Group {
+            ListSettingsPicker(title: "Ordering strategy",
+                               indexedValue: orderingAggression - 1,
+                               callback: updateOrderingAgression,
+                               labels: optionTitles)
+        }
+        .font(.body)
+    }
+
+    private func updateOrderingAgression(value: Int) {
+        let adjustedValue = value + 1
+        orderingStrategyViewModel.post(level: adjustedValue)
+    }
+}
+
+struct OrderingStrategyView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            NavigationView {
+                OrderingStrategyView(accountViewModel: AccountViewModel())
+            }
+        }
+    }
+}
