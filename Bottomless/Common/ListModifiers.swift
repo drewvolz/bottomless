d19659dcd313f11eaa839609211a08bafaeb7b69
Drew Volz <volz.drew@gmail.com>
2020-07-17 10:22:32 -0700

    Rename safeGrouped to groupedStyle
    * Remove support for below iOS 14
    * Remove fallback modifier for liststyle

--- Bottomless/Common/ListModifiers.swift
+++ Bottomless/Common/ListModifiers.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 
-@available(iOS 14.0, *)
 struct InsetGroupedViewModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
@@ -8,20 +7,8 @@ struct InsetGroupedViewModifier: ViewModifier {
     }
 }
 
-struct GroupedViewModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .listStyle(GroupedListStyle())
-            .environment(\.horizontalSizeClass, .regular)
-    }
-}
-
 extension List {
-    func safeGroupedStyle() -> some View {
-        guard #available(iOS 14, *) else {
-            return AnyView(modifier(GroupedViewModifier()))
-        }
-
-        return AnyView(modifier(InsetGroupedViewModifier()))
+    func groupedStyle() -> some View {
+        modifier(InsetGroupedViewModifier())
     }
 }
