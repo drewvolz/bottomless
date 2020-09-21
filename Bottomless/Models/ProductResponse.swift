4dabba014fc9ecaa6b235473a047c8673d668f31
Drew Volz <volz.drew@gmail.com>
2020-09-21 17:37:23 -0600

    Parse product and vendor likes in product model

--- Bottomless/Models/ProductResponse.swift
+++ Bottomless/Models/ProductResponse.swift
@@ -9,23 +9,31 @@ struct ProductResponse: Hashable, Identifiable, Decodable {
         case id = "_id"
         case name
         case vendorName = "vendor_name"
+        case vendorId = "vendor_id"
         case smallImageSrc = "small_image_src"
         case imageSrc = "image_src"
         case origin
         case description
         case roast
         case tags
+        case likes
     }
 
     var id: String?
     var name: String?
     var vendorName: String?
+    var vendorId: VendorId?
     var smallImageSrc: String?
     var imageSrc: String?
     var origin: Origin?
     var description: String?
     var roast: Roast?
     var tags: [Tags]?
+    var likes: Int
+
+    struct VendorId: Decodable, Hashable {
+        var likes: Int
+    }
 
     struct Origin: Decodable, Hashable {
         var name: String?
